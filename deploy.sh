00c91bef14e63b02b6a1ba7e1224d5dc6ecbfdea
姜冉 <jiangran@iiotos.com>
2020-07-06 17:40:17 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,110 @@
+#! /bin/bash
+#By jiangran
+#Time: 2020-04-21
+#只支持CentOS7+
+
+usage() {
+    echo "请按如下格式执行"
+    echo "USAGE: bash $0 服务器IP 现场 服务器名"
+    echo "USAGE: bash $0 10.10.10.12 高河现场 gaohe-mos"
+    exit 1
+}
+
+#入口
+if [ $# -lt 3 ]
+then
+    usage
+fi
+
+# 提示
+echo -ne "\\033[0;33m"
+cat<<EOT
+prometheus监控系统安装说明：
+    1. 此脚本只作用与首次安装的时候批量修改一些配置文件
+    2. 首次安装成后请修改sd_config下的配置文件进行定制化监控，然后重启prometheus容器
+    3. sd_config配置文件说明
+        3.1 node.yml 服务器状态监控（linux）
+        3.2 windows.yml 服务器状态监控（windows）
+        3.3 cadvisor.yml 容器状态监控（linux）
+        3.4 icmp.yml ping监控
+        3.5 tcp.yml tcp服务监控
+        3.6 http_mos.yml http服务监控
+        注： http服务监控较多，可以按照不同主机或者业务拆分成多个yml文件，拆分格式http_xxx.yml
+    4. 如果需要监控windows机器需要进行额外安装
+    5. 欢迎使用华夏天信prometheus监控系统
+EOT
+echo -ne "\\033[m"
+
+read -s -n1 -p "按任意键继续 ... "
+
+#定义一些函数
+function check_ip() {
+    IP=$1
+    if [[ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        FIELD1=$(echo $IP|cut -d. -f1)
+        FIELD2=$(echo $IP|cut -d. -f2)
+        FIELD3=$(echo $IP|cut -d. -f3)
+        FIELD4=$(echo $IP|cut -d. -f4)
+        if [ $FIELD1 -le 255 -a $FIELD2 -le 255 -a $FIELD3 -le 255 -a $FIELD4 -le 255 ]; then
+            ok "IP $IP available."
+        else
+            err "IP $IP 校验失败,请确认拿下你的IP格式是不是合法的!"
+        fi
+    else
+        err "请输入格式正确的内网IP!"
+    fi
+}
+
+err () {
+   # 打印错误消息, 并返回非0
+   # 屏幕输出使用红色字体
+   echo "$(red_echo $@)"
+   return 1
+}
+
+ok () {
+   # 打印标准输出(绿色消息), 说明某个过程执行成功, 状态码为0
+   echo "$(green_echo $@)"
+   return 0
+}
+
+log () {
+   # 打印消息, 并记录到日志, 日志文件由 LOG_FILE 变量定义
+   echo "$(blue_echo $@)"
+   return 0
+}
+
+red_echo ()      { echo -e "\033[031;1m$@\033[0m"; }
+green_echo ()    { echo -e "\033[032;1m$@\033[0m"; }
+yellow_echo ()   { echo -e "\033[033;1m$@\033[0m"; }
+blue_echo ()     { echo -e "\033[034;1m$@\033[0m"; }
+
+NET_NAME=monitor
+
+# net add
+net_flag=`docker network ls | grep -w $NET_NAME | wc -l`
+if [ $NET_NAME -eq 0 ]; then
+    docker network create $NET_NAME
+fi
+
+#校验IP
+MONI_IP=$1
+check_ip $MONI_IP
+
+MINENAME=$2
+HOSTNAME=$3
+
+# =============== 修改配置文件 ===============
+sed -i s/192.168.1.79/$MONI_IP/g $PWD/sd_config/*.yml
+sed -i s/高河测试/$MINENAME/g $PWD/sd_config/*.yml
+sed -i s/gaohe-test2/$HOSTNAME/g $PWD/sd_config/*.yml
+sed -i s/高河测试/$MINENAME/g $PWD/conf/alertmanager.yml
+
+# =============== 启动服务 ===============
+docker-compose up -d
+
+# =============== grafana安装饼图插件 ===============
+docker exec -it grafana sh -c 'grafana-cli plugins install grafana-piechart-panel'
+docker restart grafana
+
+[ $? -eq 0 ] &&  ok "ok: prometheus监控系统安装完成！" || err "error: prometheus监控系统安装失败！"
---
+++ readme.md
@@ -0,0 +1,116 @@
+### 1. 依赖环境
+
+- Cent OS 7 +
+- docker 18.03 +
+- docker-compose 1.21 +
+
+### 2. 安装步骤
+
+```shell
+# 获取软件安装包
+git clone git@git.leaniot.cn:product/monitor.git
+
+# 切换分支
+git checkout alone-monitor
+
+#创建docker网络
+docker network create monitor
+
+# 进入部署目录
+cd monitor
+
+# 启动部署脚本
+bash deploy.sh [ 服务器IP 矿名 服务器名 ]
+```
+
+### 3. 说明
+
+#### 3.1. 产品包含组件
+
+- grafana：图形展示
+
+- prometheus：监控
+
+- alertmanager：报警
+
+- alone-alert：钉钉报警
+
+- node_exporter：主机监控
+
+- cadvisor：容器监控
+
+- blackbox_exporter：网络监控
+
+- portainer：容器管理
+
+#### 3.2. 挂载信息（docker volume）
+
+- grafana：grafana_data
+- prometheus：prometheus_data
+- portainer：portainer_data
+
+#### 3.3. 网络信息（docker network）
+
+- monitor：external: true
+
+#### 3.4. 目录介绍
+
+```
+monitor/
+├── conf
+│   ├── alertmanager.yml				// alertmanager报警配置
+│   ├── grafana.ini						// grafana配置
+│   └── prometheus.yml					// prometheus配置
+├── deploy.sh							// 部署脚本
+├── docker-compose.yml					// 主程序文件
+├── moni_temp							// grafana模板
+│   ├── HTTP_TCP_ICMP监控.json
+│   ├── Windows监控.json
+│   ├── 主机监控.json
+│   └── 容器监控.json
+├── readme.md
+├── rules
+│   ├── hoststats-alert.rules			// 主机报警规则配置	
+│   └── networkstats-alet.rules			// 网络报警规则配置
+├── sd_config
+│   ├── cadvisor.yml					// 容器监控配置
+│   ├── http_mos.yml					// http监控配置
+│   ├── icmp.yml						// icmp监控配置
+│   ├── node.yml						// Linux主机监控配置
+│   ├── tcp.yml							// tcp监控配置
+│   └── windows.yml						// Windows主机监控配置
+└── secret
+    ├── grafana_password				// grafana登陆密码
+    └── portainer_password				// portainer登陆密码
+```
+
+### 4. 配置
+
+#### 4.1 监控配置
+
+- 监控信息配置
+
+> 在sd_config目录下添加相应监控内容（如需要监控主机信息，则在node.yml添加上主机即可）
+
+#### 4.2 报警配置
+
+- 报警规则配置
+
+>在rules目录下添加相应报警规则
+
+- 报警信息配置
+
+>在conf/alertmanger.yml中添加相应报警信息
+
+#### 4.3 被监控服务器配置
+
+- 监控组件配置
+
+>添加node_exporter和cadvisor等监控组件
+
+##### 相关链接
+
+[prometheus教程](https://github.com/yunlzheng/prometheus-book)
+
+[grafana模板大全](https://grafana.com/grafana/dashboards?orderBy=name&direction=asc)
+
